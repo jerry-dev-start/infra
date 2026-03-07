c0d7622f9980a55dbebde5def160fc49f2f365d5
sangGc <mCarrYoung@163.com>
2026-03-07 23:11:23 +0800

    fix:修改Mysql初始化找不到端口号的问题

--- inits/gorm_mysql.go
+++ inits/gorm_mysql.go
@@ -10,7 +10,7 @@ import (
 
 // GormMysqlInit 初始化Gorm数据库
 func GormMysqlInit() *gorm.DB {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		global.VM_CNF.MysqlConfig.Host,
 		global.VM_CNF.MysqlConfig.Port,
 		global.VM_CNF.MysqlConfig.Username,
