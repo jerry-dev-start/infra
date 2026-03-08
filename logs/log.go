4ce2b3323e7649a83b5c169f7c5d767a7fd96df6
sangGc <mCarrYoung@163.com>
2026-03-08 23:36:55 +0800

    fix:zap打印日志修改成Plain Text的方式

--- logs/log.go
+++ logs/log.go
@@ -14,12 +14,12 @@ func InitLogger() *zap.Logger {
 	// 自定义时间格式，默认是浮点数，改为人类可读格式
 	zapConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	// 关键字大写
-	zapConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	zapConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
 	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(zapConfig),   // 编码器配置
-		zapcore.AddSync(os.Stdout),          // 打印到控制台，生产环境可改为文件
-		zap.NewAtomicLevelAt(zap.InfoLevel), // 日志级别
+		zapcore.NewConsoleEncoder(zapConfig), // 编码器配置
+		zapcore.AddSync(os.Stdout),           // 打印到控制台，生产环境可改为文件
+		zap.NewAtomicLevelAt(zap.InfoLevel),  // 日志级别
 	)
 	// zap.AddCaller() 会在日志中显示调用函数的文件名和行号
 	log := zap.New(core, zap.AddCaller())
